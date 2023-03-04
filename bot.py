049e472e865d10001224a871eb0c83a0d59f7647
Arsenii Bryndin <senya.s408@gmail.com>
2023-03-04 22:38:35 +0100

    init

--- bot.py
+++ bot.py
@@ -1,118 +1,39 @@
 import os
 from dotenv import load_dotenv
-from telegram.ext import Updater, CommandHandler, PollHandler, MessageHandler, Filters
-import logging
-import poll
+from aiogram import Bot, Dispatcher, executor, types
+# from telegram.ext import Updater, CommandHandler, PollHandler, MessageHandler, Filters
+# import poll
+
 load_dotenv()
 
-token = os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN')
 
-updater = Updater(token)
-dispatcher = updater.dispatcher
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+@dp.message_handler(commands=["start"])
+async def start(message: types.Message):
+    await message.reply("Hey! I'm Julius the polyglot and I'll help you learn your new language! \n\n /help for more info")
 
-def start(update, context):
-    chat_id = update.effective_chat.id
-    
-    response = "Hey! I'm a Julius the horse and I'll help you remember new words :) \n test yourself whenever you want! \n /help for more info"
+@dp.message_handler(commands=["help"])
+async def help(message: types.Message):
+    await message.reply("Help info: \n I support all languages that wordreference.com does \n\n Type the word of the learning language to add it to your wordlist \n /quiz - answer a quiz for a random word! (wordlist in priority) \n /wordlist - shows your wordlist \n /delete *word* - deletes *word* from your wordlist")
 
-    context.bot.send_message(chat_id, response)
+@dp.message_handler(commands=["quiz"])
+async def quiz(message: types.Message):
+    await message.reply("In work")
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+@dp.message_handler(commands=["wordlist"])
+async def wordlist(message: types.Message):
+    await message.reply("In work")
 
-def help(update, context):
-    chat_id = update.effective_chat.id
-    
-    response = "Help info: \n Current supported languages: \n en -> ru \n\n Type the word of the learning language to add it to your wordlist \n /wordlist - check your wordlist \n /test - test random word \n /delete *word* - delete word form your wordlist"
+@dp.message_handler(commands=["delete"])
+async def delete(message: types.Message):
+    await message.reply("In work")
 
-    context.bot.send_message(chat_id, response)
+@dp.message_handler()
+async def word(message: types.Message):
+    await message.reply("In work")
 
-help_handler = CommandHandler('help', help)
-dispatcher.add_handler(help_handler)
-
-def wordlist(update, context):
-    chat_id = update.effective_chat.id
-    
-    if not poll.user_wordlist:
-        response = 'Your wordlist is empty :( \n Type the word in english to add it to your wordlist '
-    else: 
-        response = '\n'.join(poll.user_wordlist)
-
-    context.bot.send_message(chat_id, response)
-
-wordlist_handler = CommandHandler('wordlist', wordlist)
-dispatcher.add_handler(wordlist_handler)
-
-def test(update, context):
-    chat_id = update.effective_chat.id
-    
-    word, options, correct_option_index = poll.create_quiz()
-
-    context.bot.send_poll(chat_id, word, options, True, 'quiz', False, correct_option_index)
-
-test_handler = CommandHandler('test', test)
-dispatcher.add_handler(test_handler)
-
-def learned(update, context):
-    options = update.poll.options
-    correct_option_id = update.poll.correct_option_id
-    word = update.poll.question
-
-    counter = 0
-    answer = False
-
-    for option in options:
-        if option.voter_count == 1 and correct_option_id == counter:
-            answer = option.text
-            break
-            
-        counter += 1
-
-    if answer:
-        poll.user_learned_words.append(word)
-
-        if word in poll.user_wordlist:
-            poll.user_wordlist.remove(word) 
-
-learned_handler = PollHandler(learned, pass_chat_data=True, pass_user_data=True)
-dispatcher.add_handler(learned_handler)
-
-def delete(update, context):
-    chat_id = update.effective_chat.id
-
-    message_text = update.message.text.split(' ')
-
-    word = message_text[1] if len(message_text) >= 2 else ''
-
-    if(word.lower() in poll.user_wordlist):
-        poll.user_wordlist.remove(word.lower())
-        response = 'The word has been successfully deleted!'
-    else:
-        response = 'there\'s no ' + word + ' in the /wordlist \n example: \'/delete house\''
-
-    context.bot.send_message(chat_id, response)
-
-delete_handler = CommandHandler('delete', delete)
-dispatcher.add_handler(delete_handler)
-
-def word_addition(update, context):
-    chat_id = update.effective_chat.id
-
-    response = 'The word has been successfully added!'
-
-    if(update.message.text.lower() in poll.user_wordlist):
-        response = 'ERROR: word is already in the list'
-    elif(not(poll.get_options(update.message.text.lower()))):
-        response = 'ERROR: there\'s no ' + update.message.text + ' in the dictionary'
-    else:
-        poll.user_wordlist.append(update.message.text.lower())
-
-    context.bot.send_message(chat_id, response)
-
-word_addition_handler = MessageHandler(Filters.text & (~Filters.command), word_addition)
-dispatcher.add_handler(word_addition_handler)
-
-updater.start_polling()
-updater.idle()
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
--- readme.md
+++ readme.md
@@ -1,7 +1,7 @@
 # **julius-polyglot-bot :horse:**
 
 Telegram bot to help you improve your vocabulary
-**Created with** [python-telegram-bot](https://github.com/python-telegram-bot/python-telegram-bot)
+**Created with** [aiogram](https://github.com/aiogram/aiogram)
 
 # Usage
 
@@ -13,37 +13,20 @@ Telegram bot to help you improve your vocabulary
 # Command list
 
 `/start` - start the bot  
-`/help` - help info
+`/help` - help info  
 
-`/test` - creates a quiz
-
+`/quiz` - creates a quiz  
 `/wordlist` - shows the wordlist  
-`/delete *word*` - deletes word from the wordlist  
-`*word*` - adds word to the wordlist  
-
-# Example
-
-![0-example](https://github.com/SENYa-408/julius-polyglot-bot/blob/main/readme-imgs/screenshot-0.jpg)
-![1-example](https://github.com/SENYa-408/julius-polyglot-bot/blob/main/readme-imgs/screenshot-1.jpg)
-![2-example](https://github.com/SENYa-408/julius-polyglot-bot/blob/main/readme-imgs/screenshot-2.jpg)
+`/delete word` - deletes `word` from the wordlist  
+`word` - adds `word` to the wordlist  
 
 # Install
 
 1. Clone git repository  
-   `git clone https://github.com/SENYa-408/pepe-homework-bot.git`
-2. Go to directory with project
-3. Download all dependencies with cmd
+   `git clone https://github.com/SENYa-408/julius-polyglot-bot.git`
+2. Go to directory with project  
+3. Download all dependencies with cmd  
+   `pip install aiogram python-dotenv`  
 
-   1. `python-telegram-bot`
-   2. `logging`
-   3. `nltk`
-   4. `python-dotenv`
-   5. `request`
-   6. `random`
-
-4. Create config.env file in root directory with environment variables:  
-   `TOKEN=...` @BotFather TOKEN
-
-# Technologies used
-
-**API** - `python-telegram-bot`
+4. Create `.env` file in root directory with environment variables:  
+   `TOKEN=...` - @BotFather TOKEN
