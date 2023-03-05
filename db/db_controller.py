5d29f7b0267be1db71dec76d8a56b42fb243345b
Arsenii Bryndin <senya.s408@gmail.com>
2023-03-05 21:02:25 +0100

    db_path refactoring

--- db/db_controller.py
+++ db/db_controller.py
@@ -1,37 +1,39 @@
 import sqlite3
 from db.tables import create_tables, users, words, user_words, user_learned_words 
 
+DB_PATH = 'db/database.db'
+
 def init():
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     create_tables.init_tables(conn)
 
 # USERS
 def add_user(user_id: int):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     users.add_user(conn, user_id)
 
 def get_user(user_id: int):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     users.get_user(conn, user_id)
 
 def update_user_dict(user_id: int, value: str):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     users.update_user(conn, user_id, value, 'dictionary')
 
 def remove_user(user_id: int):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     users.remove_user(conn, user_id)
 
 # WORDLIST
 def get_random_words(num: int):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     
     res = words.get_random_words(conn, num) 
 
     return res
 
 def add_word_to_wordlist(user_id: int, word: str):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     is_learned = user_learned_words.get_user_learned_word(conn, user_id, word)
     if(is_learned):
         user_learned_words.remove_user_learned_word(conn, user_id, word)
@@ -45,12 +47,12 @@ def add_word_to_wordlist(user_id: int, word: str):
     user_words.add_user_word(conn, user_id, word)
 
 def remove_word_from_wordlist(user_id: int, word: str):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     user_words.remove_user_word(conn, user_id, word)
     user_learned_words.remove_user_learned_word(conn, user_id, word)
 
 def word_learned(user_id: int, word: str):
-    conn = sqlite3.connect('db/database.db')
+    conn = sqlite3.connect(DB_PATH)
     user_learned_words.add_user_learned_word(conn, user_id, word)
 
     user_words.remove_user_word(conn, user_id, word)
